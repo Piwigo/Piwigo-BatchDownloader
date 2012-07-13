00de504a95ac69adf855ecb60c2388eb67a46ad0
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-13 07:45:25 +0000

    [batch_downloader] Update el_GR, thanks to : bas_alba
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@16681 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/el_GR/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Προειδοποίηση:</b> όλα τα αρχεία θα διαγραφούν μέσα σε %d ώρες';
+$lang['Archive comment'] = 'Αρχειοποιημένο  σχόλιο';
+$lang['Archive prefix'] = 'Αρχειοποιημένο  πρόθεμα';
+$lang['Archives'] = 'Αρχεία';
+$lang['Batch Downloader'] = 'Πρόγραμμα λήψης αρχείων δέσμης';
+$lang['Cancel this download'] = 'Ακύρωση αυτής τη λήψης';
+$lang['Cancel this set'] = 'Ακύρωση αυτού του συνόλου';
+$lang['Delete downloads after'] = 'Διαγραφή λήψεων μετά';
+$lang['Delete this set'] = 'Διαγραφή αυτού του συνόλου';
+$lang['Download all pictures of this selection'] = 'Κατεβάστε όλες τις εικόνες αυτής της επιλογής';
+$lang['Download history'] = 'Ιστορικό Λήψεων';
+$lang['Download info'] = 'Πληροφορίες Λήψεων';
+$lang['Download links'] = 'Σύνδεσμοι Λήψεων';
+$lang['Download permissions'] = 'Δικαιώματα Λήψεων';
+$lang['Download set deleted'] = 'Το σύνολο λήψης διαγράφτηκε';
+$lang['Downloads'] = 'Λήψεις';
+$lang['Edit the set'] = 'Επεξεργαστείτε το σύνολο';
+$lang['Estimated number of archives'] = 'Εκτιμώμενος αριθμός των αρχείων';
+$lang['Estimated size'] = 'Εκτιμώμενο μέγεθος';
+$lang['Generate ZIP'] = 'Δημιουργία αρχείου ZIP';
+$lang['Maximum number of photos per download set'] = 'Μέγιστος αριθμός φωτογραφιών ανά σετ λήψης';
+$lang['Maximum photo size'] = 'Μέγιστο μέγεθος φωτογραφίας';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Μέγιστο μέγεθος του κάθε αρχείου (σε megabyte)';
+$lang['Nb archives'] = 'Nb αρχεία';
+$lang['Nb images'] = 'Nb φωτογραφίες';
+$lang['Random'] = 'Τυχαίος';
+$lang['Return to download page'] = 'Επιστροφή στην σελίδα λήψης';
+$lang['Set type'] = 'Ορίσετε τον τύπο';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'H λήψη του αρχείου ξεκίνησε, αν η λήψη δεν ξεκινήσει αυτόματα, παρακαλώ <a href="%s">κάντε κλικ εδώ</a>';
+$lang['Total size'] = 'Συνολικό μέγεθος';
+$lang['User collection'] = 'Συλλογή Χρήστη';
+$lang['User groups'] = 'Ομάδες Χρηστών';
+$lang['Whole gallery'] = 'Ολόκληρη η γκαλερί';
+$lang['You can not edit this set'] = 'Δεν μπορείτε να επεξεργαστείτε αυτό το σύνολο';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = '
+Επιλέξατε να κατεβάσετε %d φωτογραφίες, αλλά το σύστημα σας περιορίζει σε %d. Μπορείτε να επεξεργαστείτε το σύνολο, ή τις τελευταίες %d φωτογραφίες δεν θα κατέβουν.';
+$lang['done'] = 'έγινε';
+$lang['download'] = 'κατεβάστε';
+$lang['real number of archives can differ'] = 'ο πραγματικός αριθμός των αρχείων μπορεί να διαφέρει';
+?>
