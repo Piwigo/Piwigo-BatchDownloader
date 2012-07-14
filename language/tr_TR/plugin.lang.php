c831b748f8035947a21470ae7d74b99ab3036b2d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-14 12:07:26 +0000

    [batch_downloader] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@16723 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/tr_TR/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Uyarı:</b> Tüm dosyalar %d saat içinde silinecektir';
+$lang['Archive comment'] = 'Arşiv açıklaması';
+$lang['Archive prefix'] = 'Arşiv ön takısı';
+$lang['Archives'] = 'Arşivler';
+$lang['Batch Downloader'] = 'Batch Downloader';
+$lang['Cancel this download'] = 'İndirme işleminden vazgeç';
+$lang['Cancel this set'] = 'Bu gruptan vazgeç';
+$lang['Confirm the download of %d pictures?'] = '%d adet resmi indirmeyi onaylıyor musunuz?';
+$lang['Delete downloads after'] = 'İndirme sonrası sil';
+$lang['Delete this set'] = 'Bu grubu sil';
+$lang['Download all pictures of this selection'] = 'Bu seçimin tüm resimlerini indir';
+$lang['Download history'] = 'İndirme geçmişi';
+$lang['Download info'] = 'İndirme bilgisi';
+$lang['Download links'] = 'İndirme bağlantıları';
+$lang['Download permissions'] = 'İndirme izinleri';
+$lang['Download set deleted'] = 'İndirme grubu silindi';
+$lang['Downloads'] = 'İndirilenler';
+$lang['Edit the set'] = 'Grubu düzenle';
+$lang['Estimated number of archives'] = 'Tahmini arşiv sayısı';
+$lang['Estimated size'] = 'Tahmini büyüklük';
+$lang['Generate ZIP'] = 'ZIP oluştur';
+$lang['Maximum number of photos per download set'] = 'İndirme grubuna düşen maks. fotoğraf sayısı';
+$lang['Maximum photo size'] = 'Maks. fotoğraf büyüklüğü';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Her arşiv başına düşen maks. dosya büyüklüğü (MB)';
+$lang['Nb archives'] = 'Nb arşiv';
+$lang['Nb images'] = 'Nb fotoğraf';
+$lang['Random'] = 'Rasgele';
+$lang['Remove all finished downloads'] = 'Biten tüm indirmeleri kaldır';
+$lang['Remove from download set'] = 'İndirme grubundan kaldır';
+$lang['Return to download page'] = 'İndirme sayfasına dön';
+$lang['Set type'] = 'Grup tipi';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Arşiv indiriliyor, eğer indirme otomatik olarak başlamadıysa lütfen <a href="%s">buraya tıklayınız</a>';
+$lang['Total size'] = 'Toplam büyüklük';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader can\'t work without this extension.'] = 'ZipArchive PHP uzantısı bulunamadı, Yığın İndirme (Batch Downloader) bu uzantı olmadan çalışamaz.';
+$lang['User collection'] = 'Kullanıcı koleksiyonu';
+$lang['User groups'] = 'Kullanıcı grubu';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Uyarı: ZipArchive uzantısı; latin olmayan karakterler, uzantılı harfler (ĞŞ) ve açılı tırnak (») gibi özel karakterleri kabul etmez.';
+$lang['Whole gallery'] = 'Bütün galeri';
+$lang['You can not edit this set'] = 'Bu grubu düzenleyemezsiniz';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = '%d resim indirmeyi seçtiniz fakat sistem %d ile sınırlıdır. Grubu düzenleyebilirsiniz aksi halde son %d resim indirilemeyecektir.';
+$lang['done'] = 'yapıldı';
+$lang['download'] = 'indir';
+$lang['hours'] = 'saat';
+$lang['real number of archives can differ'] = 'gerçek arşiv sayısı değişebilir';
+?>
