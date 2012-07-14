8dbc74774ffebe327ffe40d10bb8fc1aa98f2cff
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-14 09:41:33 +0000

    [batch_downloader] Update it_IT, thanks to : Sugar888
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@16715 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/it_IT/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b> Attenzione: </ b> tutti i file saranno cancellati entro %d ore';
+$lang['Archive comment'] = 'Archive commento';
+$lang['Archive prefix'] = 'Archivio prefisso';
+$lang['Archives'] = 'Archivio';
+$lang['Batch Downloader'] = 'Batch downloader';
+$lang['Cancel this download'] = 'Annulla il download';
+$lang['Cancel this set'] = 'Annulla questo set';
+$lang['Confirm the download of %d pictures?'] = 'Confermare il download di %d immagini?';
+$lang['Delete downloads after'] = 'Eliminare dopo il download';
+$lang['Delete this set'] = 'Elimina questo set';
+$lang['Download all pictures of this selection'] = 'Scarica tutte le immagini di questa selezione';
+$lang['Download history'] = 'Scarica la storia';
+$lang['Download info'] = 'Scarica l/\'informazione';
+$lang['Download links'] = 'link download';
+$lang['Download permissions'] = 'Scarica le autorizzazioni';
+$lang['Download set deleted'] = 'Scarica impostare cancellato';
+$lang['Downloads'] = 'Downloads';
+$lang['Edit the set'] = 'Modificare il set';
+$lang['Estimated number of archives'] = 'Numero stimato di archivi';
+$lang['Estimated size'] = 'dimensione stimata';
+$lang['Generate ZIP'] = 'genera ZIP';
+$lang['Maximum number of photos per download set'] = 'Numero massimo di foto per il download set';
+$lang['Maximum photo size'] = 'Dimensione massima foto';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Dimensione massima di ogni archivio (in Megabyte)';
+$lang['Nb archives'] = 'Nb archivi';
+$lang['Nb images'] = 'Nb foto';
+$lang['Random'] = 'casuale';
+$lang['Remove all finished downloads'] = 'Rimuovere tutti i download terminati';
+$lang['Remove from download set'] = 'Togliere dal set del download';
+$lang['Return to download page'] = 'Torna alla pagina download';
+$lang['Set type'] = 'Imposta il tipo';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'L\'archivio sta scaricando, se il download non si avvia automaticamente si prega di <a href="%s">cliccare qui </ a>';
+$lang['Total size'] = 'dimensione totale';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader can\'t work without this extension.'] = 'Impossibile trovare ZipArchive estensione PHP, il batch downloader non può funzionare senza questa estensione.';
+$lang['User collection'] = 'raccolta dell/\'utente';
+$lang['User groups'] = 'gruppi d/\'utenti';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Attenzione: Il ZipArchive non accetta i caratteri speciali come accenti, citazioni (») e alfabeti non latini.';
+$lang['Whole gallery'] = 'intera galleria';
+$lang['You can not edit this set'] = 'Non è possibile modificare questo set';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Hai scelto di scaricare %d immagini, però il sistema è limitato a %d immagini. Puoi modificare il set o le ultime %d immagini non saranno scaricate.';
+$lang['done'] = 'finito';
+$lang['download'] = 'download';
+$lang['hours'] = 'ore';
+$lang['real number of archives can differ'] = 'numero reale degli archivi possono differire';
+?>
