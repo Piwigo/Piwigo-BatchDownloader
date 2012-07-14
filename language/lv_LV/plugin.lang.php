cbcf40dca469d9fac5fa63a96952e34e149bfdbf
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-14 16:45:53 +0000

    [batch_downloader] Update lv_LV, thanks to : agrisans
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@16732 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/lv_LV/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Uzmanību:</b> visi faili %d stundu laikā tiks izdzēsti';
+$lang['Archive comment'] = 'Arhīva komentārs';
+$lang['Archive prefix'] = 'Arhīva prefikss';
+$lang['Archives'] = 'Arhīvi';
+$lang['Batch Downloader'] = 'Grupaa Lejuplādētājs';
+$lang['Cancel this download'] = 'Atcelt šo lejuplādi';
+$lang['Cancel this set'] = 'Atcelt šo sarakstu';
+$lang['Confirm the download of %d pictures?'] = 'Apstiprināt %d attēlu lejuplādi ';
+$lang['Delete downloads after'] = 'Dzēst lejuplādes pēc';
+$lang['Delete this set'] = 'Dzēst šo sarakstu';
+$lang['Download all pictures of this selection'] = 'Lejupielādēt visas šīs atlases bildes';
+$lang['Download history'] = 'Lejuplādes vēsture';
+$lang['Download info'] = 'Lejuplādes informācija';
+$lang['Download links'] = 'Lejuplādes saites';
+$lang['Download permissions'] = 'Lejuplādes atļaujas';
+$lang['Download set deleted'] = 'Lejuplādes saraksts izdzēsts';
+$lang['Downloads'] = 'Lejuplādes';
+$lang['Edit the set'] = 'Rediģēt sarakstu';
+$lang['Estimated number of archives'] = 'Aplestais arhīvu skaits';
+$lang['Estimated size'] = 'Aplēstais izmērs';
+$lang['Generate ZIP'] = 'Izveidot ZIP';
+$lang['Maximum number of photos per download set'] = 'Maksimālais attēlu skaits lejuplādes sarakstā';
+$lang['Maximum photo size'] = 'Maksimālais attēla izmērs';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Katra arhīva maksimālais lielums (Megabaitos)';
+$lang['Nb archives'] = 'Arhīvu Nr.';
+$lang['Nb images'] = 'Attēlu Nr.';
+$lang['Random'] = 'Gadījuma';
+$lang['Remove all finished downloads'] = 'Izdzēst visas pabeigtās lejuplādes';
+$lang['Remove from download set'] = 'Izdzēst no lejuplādes saraksta';
+$lang['Return to download page'] = 'Atgriezties lejuplādes lapā';
+$lang['Set type'] = 'Iestatīt tipu';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Šis arhīvs tiek lejuplādēts, ja lejuplāde nesākas automātiski, lūdzu <a href="%s">klikšķiniet šeit</a>';
+$lang['Total size'] = 'Absolūtais izmērs';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader can\'t work without this extension.'] = 'Nav iespējams atrast ZIP Arhīva PHP paplašinājumu, Grupas Lejuplādētājs bez šī paplašinājuma nedarbojas';
+$lang['User collection'] = 'Lietotāja kolekcija';
+$lang['User groups'] = 'Lietotāju grupas';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Uzmanību: Zip Arhīvs neatbalsta speciālos simbolus, kā šādas (»)pēdiņas un nelatīņu alfabētus.';
+$lang['Whole gallery'] = 'Visa galerija';
+$lang['You can not edit this set'] = 'Jūs nevarat rediģēt šo sarakstu';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Jūs esat izvēlējies lejuplādēt %d bildes, bet sistēmā ir noteikts %d limits. Jūs varat rediģēt sarakstu vai arī beidzamās %d bildes netiks lejupielādētas.';
+$lang['done'] = 'darīts';
+$lang['download'] = 'lejuplādēt';
+$lang['hours'] = 'stundas';
+$lang['real number of archives can differ'] = 'reālais arhīvu skaits var atšķirties';
+?>
