e800aee6ca4762338385409015413767cf94ed28
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-07-18 09:53:33 +0000

    [batch_downloader] Update cs_CZ, thanks to : lanius
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@16819 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/cs_CZ/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Upozornění:</b> všechny soubory budou odstraněnyběhem %d hodin';
+$lang['Archive comment'] = 'Komentář archivu';
+$lang['Archive prefix'] = 'Prefix archivu';
+$lang['Archives'] = 'Archivy';
+$lang['Batch Downloader'] = 'Dávkové stahování';
+$lang['Cancel this download'] = 'Zrušit tot stahování';
+$lang['Cancel this set'] = 'Zrušit tuto sadu';
+$lang['Confirm the download of %d pictures?'] = 'Potvrzujete stažení %d obrázku?';
+$lang['Delete downloads after'] = 'Smazat po stažení';
+$lang['Delete this set'] = 'Smazat tuto sadu';
+$lang['Download all pictures of this selection'] = 'Stáhnout všechny obrázky z tohoto výběru';
+$lang['Download history'] = 'Historie stažení';
+$lang['Download info'] = 'Info o stažení';
+$lang['Download links'] = 'Spojení k stažení';
+$lang['Download permissions'] = 'Svolení k stažení';
+$lang['Download set deleted'] = 'Sada stažení smazána';
+$lang['Downloads'] = 'Stahování';
+$lang['Edit the set'] = 'Upravit sadu';
+$lang['Estimated number of archives'] = 'Odhadnutý počet archvů';
+$lang['Estimated size'] = 'Odhadnutá velikost';
+$lang['Generate ZIP'] = 'vytvořit ZIP';
+$lang['Maximum number of photos per download set'] = 'Maximální počet fotek na stahovanou sadu';
+$lang['Maximum photo size'] = 'Maximální velikost fota';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Maximální velikost každého archivu (v Megabytech)';
+$lang['Nb archives'] = 'Nb archivy';
+$lang['Nb images'] = 'Nb fota';
+$lang['Random'] = 'Náhodný';
+$lang['Remove all finished downloads'] = 'Odstranit všechna ukončená stahování';
+$lang['Remove from download set'] = 'Odstranit ze stažené sady';
+$lang['Return to download page'] = 'Zpět na nahranou stránku';
+$lang['Set type'] = 'Nastavit typ';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Archiv se nahrává, pokud nahrávání nezačne automaticky, prosím  <a href="%s">stiskněte zde</a>';
+$lang['Total size'] = 'Celková velikost';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader can\'t work without this extension.'] = 'Nelze najít ZipArchive PHP rozšíření, Dávkové stahování nemůže pracovat bez tohoto rozšíření';
+$lang['User collection'] = 'Uživatelský výběr';
+$lang['User groups'] = 'Uživatelské skupiny';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Upozornění: ZipArchive nemůže akceptovat speciální znaky jako jsou zdůrazněné znaky, úhlové uvozovky (») a non-latin abecedy.';
+$lang['Whole gallery'] = 'Celá galerie';
+$lang['You can not edit this set'] = 'Nemúžete editovat tuto sadu';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Vybrali jste k nahrání %d obrázků, ale systém je omezen na %d. Změňte sadu nebo posledních %d obrázků nebude nahráno.';
+$lang['done'] = 'uděláno';
+$lang['download'] = 'stáhnout';
+$lang['hours'] = 'hodiny';
+$lang['real number of archives can differ'] = 'opravdový počet archivů se může lišit';
+?>
