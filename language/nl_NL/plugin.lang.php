967770d181d92b4fecf89915ccded86f4bfb4079
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-21 14:08:12 +0000

    [batch_downloader] Update nl_NL, thanks to : hoorn666
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@17618 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nl_NL/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['done'] = 'Klaar';
+$lang['download'] = 'Download';
+$lang['hours'] = 'Uren';
+$lang['real number of archives can differ'] = 'Het werkelijke aantal archieven kan anders zijn';
+$lang['Generate ZIP'] = 'Maak ZIP';
+$lang['Maximum number of photos per download set'] = 'Maximum aantal foto\'s per download set';
+$lang['Maximum photo size'] = 'Maximale foto grootte';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Maximale grootte van elk archief (in Megabytes)';
+$lang['Nb archives'] = 'Nb archieven';
+$lang['Nb images'] = 'Nb foto\'s';
+$lang['Random'] = 'Willekeurig';
+$lang['Remove all finished downloads'] = 'Verwijder alle beëindigde downloads';
+$lang['Remove from download set'] = 'Verwijder uit download set';
+$lang['Return to download page'] = 'Ga terug naar download pagina';
+$lang['Set type'] = 'Stel type in';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Het archief wordt gedownload, als de download niet automatisch start <a href="%s">hier klikken</a> a.u.b.';
+$lang['Total size'] = 'Totale grootte';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader can\'t work without this extension.'] = 'Kan ZipArchive PHP extensie niet vinden, Batch Downloader kan zonder deze extensie niet werken ';
+$lang['User collection'] = 'Gebruiker collectie';
+$lang['User groups'] = 'Gebruiker groepen';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Waarschuwing: ZipArchive accepteert geen speciale tekens zoals accenten, groter/kleiner dan(») en non-latin karakters.';
+$lang['Whole gallery'] = 'Hele gallerij';
+$lang['You can not edit this set'] = 'U kunt deze set niet aanpassen';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Je hebt gekozen om %d plaatjes te downloaden, maar het systeem is gelimiteerd op %d. U kunt de set aanpassen of de laatste %d plaatjes worden niet gedownload.';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Waarschuwing:</b>alle bestanden worden binnen %d uur gewist';
+$lang['Archive comment'] = 'Archief commentaar';
+$lang['Archive prefix'] = 'Archief prefix';
+$lang['Archives'] = 'Archieven';
+$lang['Batch Downloader'] = 'Batch Downloader';
+$lang['Cancel this download'] = 'Voer download niet uit';
+$lang['Cancel this set'] = 'Voer deze set niet uit';
+$lang['Confirm the download of %d pictures?'] = 'Bevestig downloaden van %d plaatjes?';
+$lang['Delete downloads after'] = 'Wis downloads na';
+$lang['Delete this set'] = 'Wis deze set';
+$lang['Download all pictures of this selection'] = 'Download alle plaatjes uit deze selectie';
+$lang['Download history'] = 'Download historie';
+$lang['Download info'] = 'Download informatie';
+$lang['Download links'] = 'Download links';
+$lang['Download permissions'] = 'Download permissies';
+$lang['Download set deleted'] = 'Download set gewist';
+$lang['Downloads'] = 'Downloads';
+$lang['Edit the set'] = 'Pas de set aan';
+$lang['Estimated number of archives'] = 'Geschat aantal archieven';
+$lang['Estimated size'] = 'Geschatte grootte';
+?>
