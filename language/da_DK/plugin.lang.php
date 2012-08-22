3b8b0a08f7007bfc35699ed50766bcbb42f5ec22
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-08-22 16:50:47 +0000

    [batch_downloader] Update da_DK, thanks to : thomas1956
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@17622 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/da_DK/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Archive comment'] = 'Kommentar til arkiv';
+$lang['Archive prefix'] = 'Præfix for arkiv';
+$lang['Archives'] = 'Arkiver';
+$lang['Batch Downloader'] = 'Batch Downloader';
+$lang['Cancel this download'] = 'Fortryd denne download';
+$lang['Maximum photo size'] = 'Maksimum foto størrelse';
+$lang['Random'] = 'Tilfældig';
+$lang['Download history'] = 'Download historik';
+$lang['Download info'] = 'Download information';
+$lang['Download links'] = 'Links for download';
+$lang['Download permissions'] = 'Tilladelser for download';
+$lang['Download set deleted'] = 'Samling for download slettet';
+$lang['Downloads'] = 'Downloads';
+$lang['Edit the set'] = 'Rediger samlingen';
+$lang['Estimated number of archives'] = 'Beregnet antal zip filer';
+$lang['Estimated size'] = 'Beregnet størrelse';
+$lang['Generate ZIP'] = 'Generer zip fil';
+$lang['Cancel this set'] = 'Fortryd denne samling';
+$lang['Delete this set'] = 'Slet denne samling';
+?>
