42209079251e76003f203f058df27cb292cad373
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-02 21:26:13 +0000

    [batch_downloader] Update zh_CN, thanks to : withhaojun
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@17694 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_CN/plugin.lang.php
@@ -0,0 +1,67 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Random'] = '随机';
+$lang['Nb images'] = '特定图片';
+$lang['Nb archives'] = '特定文档';
+$lang['Remove from download set'] = '从下载设置中删除';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = '警告：ZipArchive 不接受特殊字符，例如：重音符号、引号和非拉丁字母。';
+$lang['real number of archives can differ'] = '实际文档数量可能不一致';
+$lang['hours'] = '小时';
+$lang['download'] = '下载';
+$lang['done'] = '完成';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = '你选择要下载 %d 张图片, 但是系统限制只能下载 %d 张图片。你可以编辑这个设置， 否则最后的 %d 张图片将不会被下载。';
+$lang['You can not edit this set'] = '你不能编辑这个设置';
+$lang['Whole gallery'] = '整个相册';
+$lang['User collection'] = '用户选择';
+$lang['User groups'] = '用户组';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader can\'t work without this extension.'] = '没有找到 ZipArchive 扩展，批量下载器缺少这个扩展不能工作。';
+$lang['Total size'] = '总大小';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = '文档正在下载中，如果没有开始自动下载请点击<a href="%s">这里</a>';
+$lang['Set type'] = '类型';
+$lang['Return to download page'] = '返回下载页面';
+$lang['Download set deleted'] = '删除下载设置';
+$lang['Remove all finished downloads'] = '移动所有已完成下载';
+$lang['Estimated number of archives'] = '估计文档数量';
+$lang['Downloads'] = '下载';
+$lang['Archives'] = '文档';
+$lang['Archive prefix'] = '文档前缀';
+$lang['Archive comment'] = '文档评论';
+$lang['Maximum size of each archive (in Megabytes)'] = '每个文档图片最大尺寸（M）';
+$lang['Maximum photo size'] = '最大图片大小';
+$lang['Maximum number of photos per download set'] = '每次最大下载图片数量设置';
+$lang['Generate ZIP'] = '生成ZIP';
+$lang['Estimated size'] = '估计大小';
+$lang['Edit the set'] = '编辑设置';
+$lang['Download permissions'] = '下载权限';
+$lang['Download links'] = '下载链接';
+$lang['Download info'] = '下载信息';
+$lang['Download history'] = '下载历史';
+$lang['Download all pictures of this selection'] = '下载所有选定图片';
+$lang['Delete this set'] = '删除这些设置';
+$lang['Delete downloads after'] = '稍后删除下载';
+$lang['Confirm the download of %d pictures?'] = '确定下载 %d 张图片?';
+$lang['Cancel this set'] = '取消这个设置';
+$lang['Cancel this download'] = '取消本次下载';
+$lang['Batch Downloader'] = '批量下载器';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>警告：</b> 所有文件将会在 %d 小时内被删除';
+?>
