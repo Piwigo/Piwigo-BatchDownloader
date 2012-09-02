a577e7118635057f4efd2f6aecacac710301bfbd
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-09-02 21:26:07 +0000

    [batch_downloader] Update fa_IR, thanks to : Garcia
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@17693 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/fa_IR/plugin.lang.php
@@ -0,0 +1,62 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['User collection'] = 'گسترش دهنده ی مجموعه کاربران';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>هشدار:</b> همه ی فایل ها در %d ساعت آینده پاک خواهند شد';
+$lang['Archives'] = 'آرشیو ها';
+$lang['Batch Downloader'] = 'دانلود کننده ی دسته ای';
+$lang['Cancel this download'] = 'لغو این دانلود';
+$lang['Cancel this set'] = 'لغو این دسته';
+$lang['Confirm the download of %d pictures?'] = 'دانلود %d عکس را تأیید می کنید؟';
+$lang['Delete downloads after'] = 'پاک کردن دانلود ها پس از';
+$lang['Delete this set'] = 'پاک کردن این دسته';
+$lang['Download all pictures of this selection'] = 'دانلود همه ی عکس های انتخاب شده';
+$lang['Download history'] = 'تاریخچه ی دانلود';
+$lang['Download info'] = 'اطلاعات دانلود';
+$lang['Download links'] = 'لینک های دانلود';
+$lang['Download permissions'] = 'مجوز های دانلود';
+$lang['Download set deleted'] = 'دسته ی دانلود پاک شد';
+$lang['Downloads'] = 'دانلود ها';
+$lang['Edit the set'] = 'ویرایش دسته';
+$lang['Estimated number of archives'] = 'تعداد برآورد شده ی آرشیو ها';
+$lang['Estimated size'] = 'حجم برآورد شده';
+$lang['Generate ZIP'] = 'فشرده سازی';
+$lang['Maximum number of photos per download set'] = 'بیشترین تعداد عکس های هر دسته ی دانلود';
+$lang['Maximum photo size'] = 'بیشترین مقدار حجم عکس';
+$lang['Maximum size of each archive (in Megabytes)'] = 'بیشترین مقدار حجم هر آرشیو (به مگابایت)';
+$lang['Random'] = 'تصادفی';
+$lang['Remove all finished downloads'] = 'پاک کردن همه ی دانلود های به پایان رسیده';
+$lang['Remove from download set'] = 'پاک کردن از دسته ی دانلود';
+$lang['Return to download page'] = 'بازگشت به صفحه ی دانلود';
+$lang['Set type'] = 'نوع دسته';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'آرشیو در حال دانلود می باشد، اگر دانلود بطور خودکار آغاز نشد <a href="%s">اینجا کلیک کنید</a>';
+$lang['Total size'] = 'مجموع حجم';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader can\'t work without this extension.'] = 'گشترش دهنده ی ZipArchive PHP پیدا نشد، دانلود کننده ی دسته ای نمی تواند بدون این گسترش دهنده کار کند.';
+$lang['User groups'] = 'گروه های کاربری';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'هشدار: گسترش دهنده ی ZipArchive حروف خاص مانند حروف برجسته، علامت های نقل قول (») و حروف غیرانگلیسی را قبول نمی کند.';
+$lang['Whole gallery'] = 'تمام گالری';
+$lang['You can not edit this set'] = 'شما نمی توانید این مجموعه را ویرایش کنید';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'شما %d عکس برای دانلود انتخاب کردید، ولی محدودیت دانلود تا %d عکس می باشد. می توانید مجموعه ی عکس ها را ویرایش کنید، در غیر اینصورت %d عکس آخر دانلود نخواهند شد.';
+$lang['done'] = 'انجام شد';
+$lang['download'] = 'دانلود';
+$lang['hours'] = 'ساعت';
+?>
