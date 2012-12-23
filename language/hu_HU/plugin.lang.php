b705fe1c598d60eb326125d9de114a77b8fbf21f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2012-12-23 07:01:07 +0000

    [batch_downloader] Add hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@19532 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/hu_HU/plugin.lang.php
@@ -0,0 +1,59 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2012 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Cancel this download'] = 'Letöltés megszakítása';
+$lang['Download all pictures of this selection'] = 'Az összes kiválasztott kép letöltése';
+$lang['User collection'] = 'Felhasználó készlet';
+$lang['Confirm the download of %d pictures?'] = '%d kép letöltése. Megerősíti?';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Archívum letöltése. Ha a letöltés nem indul el automatikusan, <a href="%s">kattintson ide</a>';
+$lang['Remove all finished downloads'] = 'Összes letöltés befejeztével eltávolítás';
+$lang['Remove from download set'] = 'Letöltés készlet eltávolítása';
+$lang['Archive comment'] = 'Archívum megjegyzés';
+$lang['Archive prefix'] = 'Archívum előtag';
+$lang['Archives'] = 'Archívumok';
+$lang['Delete downloads after'] = 'Törlés letöltés után';
+$lang['Maximum number of photos per download set'] = 'Képek maximális száma / letöltés készlet';
+$lang['Maximum photo size'] = 'Maximális képméret';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Archívum maximális mérete (Megabájtban)';
+$lang['Number of archives'] = 'Archívumok száma';
+$lang['Number of images'] = 'Képek száma';
+$lang['User groups'] = 'Felhasználói csoportok';
+$lang['Whole gallery'] = 'Teljes galéria';
+$lang['done'] = 'kész';
+$lang['download'] = 'letöltés';
+$lang['hours'] = 'óra';
+$lang['Total size'] = 'Teljes méret';
+$lang['Set type'] = 'Típus kiválasztása';
+$lang['Generate ZIP'] = 'ZIP fájl létrehozása';
+$lang['Random'] = 'Véletlen';
+$lang['Return to download page'] = 'Vissza a letöltési oldalra';
+$lang['Batch Downloader'] = 'Kötegelt letöltő';
+$lang['Delete this set'] = 'Készlet törlése';
+$lang['Download history'] = 'Letöltés előzmények';
+$lang['Download info'] = 'Letöltés adatok';
+$lang['Download links'] = 'Letöltés hivatkozások';
+$lang['Download permissions'] = 'Letöltés engedélyek';
+$lang['Download set deleted'] = 'Letöltés készlet törölve';
+$lang['Downloads'] = 'Letöltések';
+$lang['Edit the set'] = 'Készlet szerkesztése';
+$lang['Estimated size'] = 'Becsült méret';
+?>
