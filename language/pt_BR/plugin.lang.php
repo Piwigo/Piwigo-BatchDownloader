7f2e879219fe41455f8a289b9e9b7b51132cd026
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-24 20:43:03 +0000

    [batch_downloader] Add pt_BR, thanks to : flaviove
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@20364 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/pt_BR/plugin.lang.php
@@ -0,0 +1,68 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Você escolheu para baixar as imagens %d, mas o sistema é limitado a %d. Você pode editar o conjunto %d, ou as últimas imagens últimos %d não serão baixada.';
+$lang['done'] = 'concluído';
+$lang['download'] = 'download';
+$lang['hours'] = 'horas';
+$lang['real number of archives can differ'] = 'número real de arquivos que podem diferir';
+$lang['Generate ZIP'] = 'Gerar ZIP';
+$lang['Maximum number of photos per download set'] = 'Número máximo de fotos por conjunto de Download';
+$lang['Maximum photo size'] = 'Tamanho máximo de foto';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Tamanho máximo de cada arquivo (em Megabytes)';
+$lang['Number of archives'] = 'Número de arquivos';
+$lang['Number of images'] = 'Número de fotos';
+$lang['Random'] = 'Aleatóriamente';
+$lang['Remove all finished downloads'] = 'Remova todos os downloads concluídos';
+$lang['Remove from download set'] = 'Remova do conjunto de Download';
+$lang['Return to download page'] = 'Retornar à página de download';
+$lang['Set type'] = 'Definir o tipo';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Iniciando Download do Archive #%d vai destruir o Arquivo #%d, certifique-se de terminar o download. Continuar?';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'O arquivo está sendo baixado, se o download não começar automaticamente, por favor <a href="%s">clique aqui</ a>';
+$lang['Total size'] = 'Tamanho total';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'Incapaz de encontrar a extensão PHP ZipArchive, o urilitário Batch Downloader usará no lugar PclZip, mas com um pior desempenho.';
+$lang['User collection'] = 'Coleção de usuário';
+$lang['User groups'] = 'Crupos de usuários';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Aviso: ZipArchive não aceita caracteres especiais, como os acentuados, citações angulares (») e alfabetos não latinos.';
+$lang['Whole gallery'] = 'Toda a galeria';
+$lang['You can not edit this set'] = 'Voce não pode editar esta configuração';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Aviso:</b> todos os arquivos serão apagados dentro de %d horas';
+$lang['Archive comment'] = 'Arquivar comentário';
+$lang['Archive prefix'] = 'Prefixo do arquivo';
+$lang['Archives'] = 'Arquivos';
+$lang['Batch Downloader'] = 'Utilitário de Download';
+$lang['Cancel this download'] = 'Cancelar este download';
+$lang['Cancel this set'] = 'Cancelar este conjunto';
+$lang['Confirm the download of %d pictures?'] = 'Confirme o download das imagens %d?';
+$lang['Delete downloads after'] = 'Excluir downloads após';
+$lang['Delete this set'] = 'Excluir este conjunto';
+$lang['Download all pictures of this selection'] = 'Baixe todas as imagens desta seleção';
+$lang['Download history'] = 'Baixe história';
+$lang['Download info'] = 'Baixar informação de transferência';
+$lang['Download links'] = 'Baixar links';
+$lang['Download permissions'] = 'Permissões de download';
+$lang['Download set deleted'] = 'Baixe conjunto excluído';
+$lang['Downloads'] = 'Downloads';
+$lang['Edit the set'] = 'Editar o conjunto';
+$lang['Estimated number of archives'] = 'Número estimado de arquivos';
+$lang['Estimated size'] = 'Tamanho estimado';
+?>
