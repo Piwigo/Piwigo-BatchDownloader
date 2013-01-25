025124188741534ae051ae4a565762d5cf3fffaa
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-25 17:40:25 +0000

    [batch_downloader] Update da_DK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@20380 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/da_DK/plugin.lang.php
+++ language/da_DK/plugin.lang.php
@@ -65,4 +65,5 @@ $lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip
 $lang['Number of archives'] = 'Antal arkiver';
 $lang['Number of images'] = 'Antal fotografier';
 $lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Hvis du begynder at hente Archive #%d, vil Archive #%d blive slettet, så vær sikker på at din download er afsluttet. Fortsæt?';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Advarsel: Kun registrerede brugere kan benytte Batch Downloader.';
 ?>
