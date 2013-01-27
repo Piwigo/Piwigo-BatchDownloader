9458f8323250a83746f97f9e02a1150fc0a7cdec
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-27 12:48:36 +0000

    [batch_downloader] Update ru_RU, thanks to : yuragal
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@20416 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -65,4 +65,5 @@ $lang['hours'] = 'часов';
 $lang['real number of archives can differ'] = 'реальное число архивов может отличаться';
 $lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'Невозможно найти PHP расширение ZipArchive, вместо него Batch Downloader будет использовать PclZip, что приведет к потере производительности.  ';
 $lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Запуск загрузки архива #%d уничтожит архив #%d, убедитесь, что вы закончите загрузку. Продолжить?';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Предупреждение: Только зарегистрированные пользователи могут использовать Batch Downloader';
 ?>
