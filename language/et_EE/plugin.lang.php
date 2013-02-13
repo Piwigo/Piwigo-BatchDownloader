44c4cfaecdac6915b12f5d102f730818e3e4132f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-02-13 18:54:19 +0000

    [batch_downloader] Add et_EE, thanks to : kersta
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@20744 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/et_EE/plugin.lang.php
@@ -0,0 +1,69 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You can not edit this set'] = 'Sa ei või toimetada seda partiid';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Sa valisid allalaadimiseks %d pilti, aga süsteemi piirang on %d. Sa võid muuta partiid, või viimased %d pilti jäävad alla laadimata.';
+$lang['done'] = 'valmis';
+$lang['download'] = 'lae alla';
+$lang['hours'] = 'tundi';
+$lang['real number of archives can differ'] = 'tegelik arhiivide arv võib erineda';
+$lang['Generate ZIP'] = 'Moodusta ZIP';
+$lang['Maximum number of photos per download set'] = 'Maksimaalne fotode arv ühes allalaadimise partiis';
+$lang['Maximum photo size'] = 'Maksimaalne foto suurus';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Iga arhiivi maksimaalne suurus (megabaitides)';
+$lang['Number of archives'] = 'Arhiivide arv';
+$lang['Number of images'] = 'Fotode arv';
+$lang['Random'] = 'Juhuslik';
+$lang['Remove all finished downloads'] = 'Eemalda kõik lõpetatud allalaadimised';
+$lang['Remove from download set'] = 'Eemalda allalaadimise partiist';
+$lang['Return to download page'] = 'Tagasi allalaadimise lehele';
+$lang['Set type'] = 'Partii tüüp';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Arhiivi allalaadimise alustamine #%d hävitab Arhiivi #%d, veendu et sa lõpetad allalaadimise. Jätkata?';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Arhiivi laetakse alla, kui laadimine ei alga automaatselt, palun <a href="%s">vajuta siia</a>';
+$lang['Total size'] = 'Kogu suurus';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'Ei ole võimalik leida ZipArchive PHP laiendit, Batch Downloader kasutab PclZip selle asemel, kuid see toimib halvemini.';
+$lang['User collection'] = 'Kasutaja valik';
+$lang['User groups'] = 'Kasutajate grupid';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Hoiatus: ainult registreeritud kasutajad saavad kasutada Batch Downloaderit.';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Hoiatus: ZipArchive ei tunnista spetsiaalseid märke, nagu nurga jutumärgid (») ja mitte-ladina tähestikku.';
+$lang['Whole gallery'] = 'Kogu galerii';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Hoiatus:</b> kõik failid kustutatakse %d tunni jooksul';
+$lang['Archive comment'] = 'Arhiivi kommentaar';
+$lang['Archive prefix'] = 'Arhiivi eesliide';
+$lang['Archives'] = 'Arhiivid';
+$lang['Batch Downloader'] = 'Hulgi allalaadija';
+$lang['Cancel this download'] = 'Tühista see allalaadimine';
+$lang['Cancel this set'] = 'Tühista see partii';
+$lang['Confirm the download of %d pictures?'] = 'Kinnita %d pildi allalaadimine';
+$lang['Delete downloads after'] = 'Kustuta hiljem allalaadimised';
+$lang['Delete this set'] = 'Kustuta see partii';
+$lang['Download all pictures of this selection'] = 'Lae alla kõik pildid sellest valikust';
+$lang['Download history'] = 'Allalaadimise ajalugu';
+$lang['Download info'] = 'Allalaadimise info';
+$lang['Download links'] = 'Allalaadimise lingid';
+$lang['Download permissions'] = 'Allalaadimise õigused';
+$lang['Download set deleted'] = 'Allalaadimise partii on kustutatud';
+$lang['Downloads'] = 'Allalaadimised';
+$lang['Edit the set'] = 'Toimeta partiid';
+$lang['Estimated number of archives'] = 'Arvestuslik arhiivide arv';
+$lang['Estimated size'] = 'Arvestuslik suurus';
+?>
