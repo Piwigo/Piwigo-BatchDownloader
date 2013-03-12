9a932cfec51290a4847df8a7c58e9dcc2122585d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-12 08:41:26 +0000

    [batch_downloader] Add bg_BG, thanks to : picsvet
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@21501 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/bg_BG/plugin.lang.php
@@ -0,0 +1,70 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Remove from download set'] = 'Махни от изтегления набор';
+$lang['Confirm the download of %d pictures?'] = 'Потвърдете изтеглянията на  %d снимките?';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Можете да изберете да изтеглите %d снимки, но системата е ограничена до %d. Можете да редактирате набора или последните %d  снимки няма да бъдат изтеглени.';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Внимание: ZipArchive не приема специални символи като подчертаните, ъглови кавички (") и не с латински букви.';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'Не мога да намеря ZipArchive PHP разширение, Batch Downloader ще използва PclZip вместо това, но с влошени показатели.';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Архивът се изтегля, ако изтеглянето не започне автоматично моля <a href="%s">кликнете тук</a>';
+$lang['real number of archives can differ'] = 'реалния брой на архивите може да се различава';
+$lang['hours'] = 'часове';
+$lang['Download set deleted'] = 'Изтегления набор е изтрит';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Максимален размер на всеки архив (в мегабайти)';
+$lang['Maximum photo size'] = 'Максимален размер снимка';
+$lang['Number of archives'] = 'Номер на архивите';
+$lang['Maximum number of photos per download set'] = 'Максимален брой на снимките за изтегляне';
+$lang['Generate ZIP'] = 'Генериране на ZIP';
+$lang['Downloads'] = 'Изтегляния';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Предупреждение:</b>всички файлове ще бъдат изтрити рамките %d часа';
+$lang['download'] = 'изтеглите';
+$lang['done'] = 'направено';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Внимание: Само регистрирани потребители могат да използват Batch Downloader.';
+$lang['Total size'] = 'Краен размер';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Стартиране на изтегляне Архив #%d ще унищожи Архив #%d, че сте завършили изтеглянето. Искате ли да продължите?';
+$lang['Return to download page'] = 'Назад към страницата за изтегляне';
+$lang['You can not edit this set'] = 'Вие не можете да редактирате този набор';
+$lang['Whole gallery'] = 'Цялата галерия';
+$lang['What can be downloaded?'] = 'Какво може да бъде свалено?';
+$lang['Estimated size'] = 'Приблизителният размер';
+$lang['Estimated number of archives'] = 'Очакван брой на архивите';
+$lang['Edit the set'] = 'Редактиране на набор';
+$lang['User groups'] = 'Потребителски групи';
+$lang['User collection'] = 'Потребителски колекции';
+$lang['Download permissions'] = 'Разрешения за изтегляне';
+$lang['Download links'] = 'Изтегляне на връзки';
+$lang['Download info'] = 'Информация за свалянето';
+$lang['Download history'] = 'Изтегляне на историята';
+$lang['Download all pictures of this selection'] = 'Сваляйте всякакви снимки на тази селекция';
+$lang['Set type'] = 'Задайте тип';
+$lang['Number of images'] = 'Брой снимки';
+$lang['Random'] = 'Случаен';
+$lang['Remove all finished downloads'] = 'Премахнете всички готови сваляния';
+$lang['Delete this set'] = 'Изтриване на този набор';
+$lang['Delete downloads after'] = 'Изтриване на сваляния след';
+$lang['Batch Downloader'] = 'Batch Downloader';
+$lang['Cancel this set'] = 'Закриване на този набор';
+$lang['Cancel this download'] = 'Откажи това изтегляне';
+$lang['Archives'] = 'Архив';
+$lang['Archive prefix'] = 'Архив префикс';
+$lang['Archive comment'] = 'Архив коментар';
+?>
