4272a1d22122900e16505b8d197b128b853db6df
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-03-16 05:07:16 +0000

    [batch_downloader] Update pl_PL, thanks to : T00kie
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@21594 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/pl_PL/plugin.lang.php
+++ language/pl_PL/plugin.lang.php
@@ -66,4 +66,5 @@ $lang['download'] = 'pobierz';
 $lang['real number of archives can differ'] = 'rzeczywista liczba archiwów może się różnić';
 $lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Rozpoczynam ściąganie archiwum #%d zniszczy archiwum #%d, Upewnij się, że ukończono ściąganie. Kontynuować ?';
 $lang['Warning: Only registered users can use Batch Downloader.'] = 'UWAGA: Tylko zarejestrowani użytkownicy mogą korzystać z Batch Downloader';
+$lang['What can be downloaded?'] = 'Co może zostać pobrane?';
 ?>
