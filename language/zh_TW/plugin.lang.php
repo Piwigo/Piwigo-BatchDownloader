ee7ce094c4f8059f7607ab8009b290b33538fc64
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-03 06:29:46 +0000

    [batch_downloader] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@21988 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,71 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Generate ZIP'] = '產生 ZIP';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = '找不到ZipArchive PHP擴展，批量下載使用PclZip代替，但性能下降。';
+$lang['User collection'] = '使用者收集本';
+$lang['User groups'] = '用戶群組';
+$lang['Warning: Only registered users can use Batch Downloader.'] = '警告：只有註冊用戶可以使用批量下載。';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = '警告：ZipArchive不接受特殊字符，如突顯的角引號(») 和非拉丁字母。';
+$lang['Remove all finished downloads'] = '刪除所有已完成的下載';
+$lang['Remove from download set'] = '刪除下載檔案集';
+$lang['Return to download page'] = '返回到下載頁面';
+$lang['Set type'] = '集本類型';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = '開始下載歸檔 #%d 會破壞歸檔 #%d，確定你是否完成下載。要繼續嗎？';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = '檔案下載，如果下載沒有自動啟動，請<a href="%s">單擊此處</a>';
+$lang['Total size'] = '總計大小';
+$lang['Maximum number of photos per download set'] = '每組照片下載的最大數量';
+$lang['Maximum photo size'] = '照片最大尺寸';
+$lang['Maximum size of each archive (in Megabytes)'] = '每個歸檔文件最大尺寸 (以MB為單位)';
+$lang['No result'] = '沒有結果';
+$lang['Number of archives'] = '歸檔數量';
+$lang['Number of images'] = '照片數量';
+$lang['Random'] = '隨機';
+$lang['Download all pictures of this selection'] = '下載這個選擇所有的圖片';
+$lang['Download history'] = '下載記錄';
+$lang['Download info'] = '下載資訊';
+$lang['Download links'] = '下載連結';
+$lang['Download permissions'] = '下載權限';
+$lang['Download set deleted'] = '下載組已刪除';
+$lang['Downloads'] = '下載';
+$lang['Edit the set'] = '編輯組';
+$lang['Estimated number of archives'] = '估計數量歸檔';
+$lang['Estimated size'] = '估計尺寸';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b> 警告：</b>內的所有文件都將被刪除 %d 小時';
+$lang['Archive comment'] = '評論歸檔';
+$lang['Archive prefix'] = '前綴歸檔';
+$lang['Archives'] = '歸檔';
+$lang['Batch Downloader'] = '批量下載';
+$lang['Cancel this download'] = '取消這個下載';
+$lang['Cancel this set'] = '取消這組';
+$lang['Confirm the download of %d pictures?'] = '確認下載中 %d 張圖片嗎？';
+$lang['Delete downloads after'] = '下載之後刪除';
+$lang['Delete this set'] = '刪除這組';
+$lang['What can be downloaded?'] = '有什麼可以下載？';
+$lang['Whole gallery'] = '整個畫廊';
+$lang['You can not edit this set'] = '您無法編輯這組';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = '您可以選擇下載 %d 張圖片，但系統限於 %d。您可以編輯本組，或 %d 張圖片將不會被下載。';
+$lang['done'] = '完成';
+$lang['download'] = '下載';
+$lang['hours'] = '時間';
+$lang['real number of archives can differ'] = '歸檔有所不同的實數';
+?>
