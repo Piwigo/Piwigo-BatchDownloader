a2a8333375307c635bafc42393c6682895e1716d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-05-08 08:06:52 +0000

    [batch_downloader] Add sv_SE, thanks to : stefan63
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@22542 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sv_SE/plugin.lang.php
@@ -0,0 +1,72 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['What can be downloaded?'] = 'Vad kan laddas ned?';
+$lang['Whole gallery'] = 'Hela galleri';
+$lang['You can not edit this set'] = 'Du kan inte redigera det här paketet';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Du valde att ladda ned %d bilder, men systemet är begränsat till %d. Om du inte redigerar paketet så kommer de sist tillagda filerna att uteslutas från nedladdningen.';
+$lang['done'] = 'klar';
+$lang['download'] = 'ladda ned';
+$lang['hours'] = 'timmar';
+$lang['real number of archives can differ'] = 'verkligt antal arkiv kan variera';
+$lang['Generate ZIP'] = 'Skapa ZIP fil';
+$lang['Maximum number of photos per download set'] = 'Max antal bilder/nedladdnings paket';
+$lang['Maximum photo size'] = 'Max bild storlek';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Max storlek/arkiv (i MB)';
+$lang['No result'] = 'Inga resultat';
+$lang['Number of archives'] = 'Antal arkiv';
+$lang['Number of images'] = 'Antal bilder';
+$lang['Random'] = 'Slumpmässig';
+$lang['Remove all finished downloads'] = 'Ta bort alla färdiga nedladdningar';
+$lang['Remove from download set'] = 'Ta bort från dedladdningspaket';
+$lang['Return to download page'] = 'Återgå till nedladdningssida';
+$lang['Set type'] = 'Paket typ';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Start av arkiv  nedladdning #%d förstör arkiv #%d, är du säker på att du vill avsluta nedladdning.
+Fortsätt?';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Arkivet laddas ned, om nedladdningen inte startar automatiskt vänligen <a href="%s">Klicka här</a>';
+$lang['Total size'] = 'Total storlek';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'ZipArchive PHP extension kan inte hittas, Batch nedladdaren kommer att använda PclZip istället, men då med lägre prestanda.';
+$lang['User collection'] = 'Användarsamling';
+$lang['User groups'] = 'Använargrupper';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Varning: Bara registrerade användare kan bruka Batch Nedladdaren.';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Varning: Zip arkiv accepterar inte specialtecken i filnam.';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Varning:</b> alla filer raderas innom %d timmar';
+$lang['Archive comment'] = 'Arkiv komentar';
+$lang['Archive prefix'] = 'Arkiv prefix';
+$lang['Archives'] = 'Arkiv';
+$lang['Batch Downloader'] = 'Batch nedladdare';
+$lang['Cancel this download'] = 'Avbryt nedladdning';
+$lang['Cancel this set'] = 'Avbryt det här paketet';
+$lang['Confirm the download of %d pictures?'] = 'Bekräfta dedladdning av %d bilder?';
+$lang['Delete downloads after'] = 'Radera nedladdningar efter';
+$lang['Delete this set'] = 'Radera det här paketet';
+$lang['Download all pictures of this selection'] = 'Ladda ned alla valda bilder';
+$lang['Download history'] = 'Nedladdnings historik';
+$lang['Download info'] = 'Nedladdnings info';
+$lang['Download links'] = 'Nedladdnings länkar';
+$lang['Download permissions'] = 'Nedladdnings befogenhet';
+$lang['Download set deleted'] = 'Nedladdnings paket raderat';
+$lang['Downloads'] = 'Nedladdningar';
+$lang['Edit the set'] = 'Redigera paket';
+$lang['Estimated number of archives'] = 'Beräknat antal arkiv';
+$lang['Estimated size'] = 'Beräknad storlek';
+?>
