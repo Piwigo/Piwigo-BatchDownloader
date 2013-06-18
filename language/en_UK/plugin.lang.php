5002bde192086aa0ad900cd063c97f7432a60a0d
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-18 17:40:04 +0000

    [batch_downloader] Update en_UK, thanks to : Kaare
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@23331 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/en_UK/plugin.lang.php
+++ language/en_UK/plugin.lang.php
@@ -51,7 +51,7 @@ $lang['No result'] = 'No result';
 $lang['Sorry, there is nothing to download. Some files may have been excluded because of <i title="Authorized types are : %s">filetype restrictions</i>.'] = 'Sorry, there is nothing to download. Some files may have been excluded because of <i title="Authorized types are : %s">filetype restrictions</i>.';
 $lang['Unknown'] = 'Unknown';
 $lang['Preparation'] = 'Preparation';
-$lang['Please wait, your download is being prepared. This page will refresh automatically refresh when it is ready.'] = 'Please wait, your download is being prepared. This page will refresh automatically refresh when it is ready.';
+$lang['Please wait, your download is being prepared. This page will refresh automatically refresh when it is ready.'] = 'Please wait, your download is being prepared. This page will refresh automatically when it is ready.';
 $lang['%d MB'] = '%d MB';
 $lang['Archive #%d (already downloaded)'] = 'Archive #%d (already downloaded)';
 $lang['Archive #%d (ready)'] = 'Archive #%d (ready)';
