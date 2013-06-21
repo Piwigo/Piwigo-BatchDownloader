b63cfbbde4a4ec57e03a4da57739b13156df079b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-06-21 04:19:23 +0000

    [batch_downloader] Update hu_HU, thanks to : samli
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@23422 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -57,7 +57,7 @@ $lang['Downloads'] = 'Letöltések';
 $lang['Edit the set'] = 'Készlet szerkesztése';
 $lang['Estimated size'] = 'Becsült méret';
 $lang['You can not edit this set'] = 'Nem tudja szerkeszteni a készletet';
-$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Figyelem:</b> azösszes fájl törlésre kerül %d órán belül';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Figyelem:</b> az összes fájl törlésre kerül %d órán belül';
 $lang['Estimated number of archives'] = 'Archívum becsült száma';
 $lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = '%d képet választott ki letöltésre, de a rendszer csak %d képet tud letölteni. Szerkessze a készletet, vagy az utolsó %d kép nem fog letöltődni.';
 $lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'Nem található ZipArchive PHP kiterjesztés, a Batch Downloader a pclzip-et fogja használni helyette, de csökkenti a teljesítményt.';
@@ -80,4 +80,5 @@ $lang['Don\'t download archives through PHP'] = 'Ne töltsön le archívumokat P
 $lang['It saves space on the server but doesn\'t allow to restart failed downloads.'] = 'Helyet takarít meg a szerveren, de nem teszi lehetővé a sikertelen letöltések újraindítását.';
 $lang['Only check if you are experiencing corrupted archives with ZipArchive.'] = 'Csak akkor használja, ha azt tapasztalja, hogy sérült a ZipArchive archívum.';
 $lang['Only check if your host complains about high PHP usage.'] = 'Csak akkor válassza, ha a szolgáltató reklamál a magas PHP hatnálat miatt.';
+$lang['Sorry, there is nothing to download. Some files may have been excluded because of <i title="Authorized types are : %s">filetype restrictions</i>.'] = 'Sajnálom, nincs mit letölteni. Egyes fájlok kizárásra kerültek. <i title="Authorized types are : %s">nem támogatott fájltípus</i>.';
 ?>
