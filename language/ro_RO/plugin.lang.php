863c4d9d3a6580d7dd15b24bba6e1bc57390d067
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-07-12 15:23:29 +0000

    [batch_downloader] Add ro_RO, thanks to : smarmie
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@23930 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ro_RO/plugin.lang.php
@@ -0,0 +1,85 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Only check if you are experiencing corrupted archives with ZipArchive.'] = 'Bifează numai dacă ai arhive corupte cu ZipArchive.';
+$lang['Only check if your host complains about high PHP usage.'] = 'Bifează numai dacă gazda se plănge de utilizare ridicată a PHP.';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Începerea descărcării Arhivei #%d va distruge Arhiva #%d, asigură-te că ai terminat descărcarea. Continuă?';
+$lang['Archive comment'] = 'Comentariu arhivă';
+$lang['Confirm the download of %d pictures?'] = 'Confirmi descărcarea a %d imagini?';
+$lang['Force the usage of PclZip instead of ZipArchive as ZIP library'] = 'Impune utilizarea PclZip ca librărie ZIP în locul lui ZipArchive';
+$lang['It saves space on the server but doesn\'t allow to restart failed downloads.'] = 'Economisește spațiu pe server, dar nu permite reinițializarea descărcărilor întrerupte';
+$lang['Please wait, your download is being prepared. This page will automatically refresh when it is ready.'] = 'Așteaptă, descărcarea ta este în curs de pregătire. Această pagina va fi reîmprospătată automat când este gata.';
+$lang['Set type'] = 'Alege tipul';
+$lang['Sorry, there is nothing to download. Some files may have been excluded because of <i title="Authorized types are : %s">filetype restrictions</i>.'] = 'Îmi pare rău, nu există nimic de descărcat. Unele fișiere s-ar putea să fi fost exclude, din cauza <i title="Tipurile autorizare sunt : %s">Restricțiilor de fișiere</i>.';
+$lang['User collection'] = 'Colecții utilizator';
+$lang['User groups'] = 'Grupuri utilizator';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Arhiva se descarcă, dacă descărcarea nu începe automat, <a href="%s">apasă aici</a>';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'Nu pot găsi extensia PHP ZipArchive, Descărcarea de Loturi va folosi PlcZip, dar cu performanță redusă.';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Atenție: ZipArchive nu acceptă caractere speciale, ca accente, paranteze unghiulare (») si alfabeturi ne-latinice.';
+$lang['Generate ZIP'] = 'Generează ZIP';
+$lang['Maximum photo size'] = 'Dimensiunea maximă a fotografiei';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Mărimea maximă a fiecărei arhive (în Megabiți)';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Atenție:</b> toate fișierele vor fi șterse în %d ore';
+$lang['Archive #%d (already downloaded)'] = 'Arhiva #%d (deja descărcată)';
+$lang['Archive #%d (pending)'] = 'Arhiva #%d (în așteptare)';
+$lang['Archive #%d (ready)'] = 'Arhiva #%d (gata)';
+$lang['Batch Downloader'] = 'Descărcare de loturi';
+$lang['Download all pictures of this selection'] = 'Descarcă toate imaginile acestei selecții';
+$lang['Download info'] = 'Informații descărcări';
+$lang['Download set deleted'] = 'Setul de descărcat a fost șters';
+$lang['%d MB'] = '%d MB';
+$lang['Archive prefix'] = 'Prefix arhive';
+$lang['Archives'] = 'Arhive';
+$lang['Cancel this download'] = 'Anulează această descărcare';
+$lang['Cancel this set'] = 'Anulează acest set';
+$lang['Delete downloads after'] = 'Șterge arhivele după';
+$lang['Delete previous archive when starting to download another one'] = 'Șterge arhiva precedentă când începe descărcarea uneia noi';
+$lang['Delete this set'] = 'Șterge acest set';
+$lang['Don\'t download archives through PHP'] = 'Nu descărca această arhiva prin PHP';
+$lang['Download history'] = 'Istoric descărcări';
+$lang['Download links'] = 'Legături descărcări';
+$lang['Download permissions'] = 'Permisiuni de descărcare';
+$lang['Downloads'] = 'Descărcări';
+$lang['Edit the set'] = 'Modifică setul';
+$lang['Estimated number of archives'] = 'Număr estimat de arhive';
+$lang['Estimated size'] = 'Mărime estimată';
+$lang['Maximum number of photos per download set'] = 'Număr maxim de fotografii în ';
+$lang['No result'] = 'Niciun rezultat';
+$lang['Number of archives'] = 'Număr de arhive';
+$lang['Number of images'] = 'Număr de fotografii';
+$lang['Preparation'] = 'Pregătire';
+$lang['Random'] = 'Aleator';
+$lang['Remove all finished downloads'] = 'Șterge toate descărcările terminate';
+$lang['Remove from download set'] = 'Șterge din setul de descărcări';
+$lang['Return to download page'] = 'Înapoi la pagina de descărcări';
+$lang['Total size'] = 'Mărime totală';
+$lang['Unknown'] = 'Necunoscut';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Atenție: Numai utilizatorii înregistrați pot utiliza Descărcarea de Loturi.';
+$lang['What can be downloaded?'] = 'Ce poate fi descărcat?';
+$lang['Whole gallery'] = 'Întreaga galerie';
+$lang['You can not edit this set'] = 'Nu poți modifica acest set';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Ai ales să descarci %d imagini, dar sistemul este limitat la %d. Poti modifica setul, sau ultimele %d imagini nu vor fi descărcate.';
+$lang['done'] = 'gata';
+$lang['download'] = 'descărcare';
+$lang['hours'] = 'ore';
+$lang['real number of archives can differ'] = 'numărul real de arhive poate diferi';
+?>
