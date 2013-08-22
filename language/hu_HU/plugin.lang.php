c0d6764df99383c8a6504c31aac19e77017a17f9
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-08-22 16:09:36 +0000

    [batch_downloader] Update hu_HU, thanks to : mionix
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@24219 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/hu_HU/plugin.lang.php
+++ language/hu_HU/plugin.lang.php
@@ -81,4 +81,5 @@ $lang['It saves space on the server but doesn\'t allow to restart failed downloa
 $lang['Only check if you are experiencing corrupted archives with ZipArchive.'] = 'Csak akkor használja, ha azt tapasztalja, hogy sérült a ZipArchive archívum.';
 $lang['Only check if your host complains about high PHP usage.'] = 'Csak akkor válassza, ha a szolgáltató reklamál a magas PHP hatnálat miatt.';
 $lang['Sorry, there is nothing to download. Some files may have been excluded because of <i title="Authorized types are : %s">filetype restrictions</i>.'] = 'Sajnálom, nincs mit letölteni. Egyes fájlok kizárásra kerültek. <i title="Authorized types are : %s">nem támogatott fájltípus</i>.';
+$lang['Force the usage of PclZip instead of ZipArchive as ZIP library'] = 'PclZip használatának kényszerítése a ZipArchive helyett, mint ZIP könyvtár';
 ?>
