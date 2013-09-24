8bbe0d30ff100463cb6861d6fbeadc87ec5a0f2b
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-09-24 22:14:35 +0000

    [batch_downloader] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@24614 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,85 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Please wait, your download is being prepared. This page will automatically refresh when it is ready.'] = 'Bonvolu atendi, via elŝuto prepariĝas. Tiu paĝo aŭtomate aktualigos, kiam pretos.';
+$lang['Preparation'] = 'Preparado';
+$lang['Random'] = 'Hazarda';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Komenciĝi elŝuti la arĥivon #%d detruos la arĥivon #%d. Certigu, ke la elŝuto finiĝos antaŭ ol pludaŭri.';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Tiu arĥivo estas elŝutata, se la elŝuto ne komenciĝas aŭtomate, bonvolu <a href="%s">musklaki ĉi-tien</a>';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'Ne eblas trovi la PHP-kromprogramo de ZipArchive, la stapla elŝutilo do uzos PclZip anstataŭe, sed kun pli malbonaj efikecoj.';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Atentu: ZipArchive ne akceptas specialajn signojn, ekz-e supersignitajn literojn, citilojn kaj nelatinajn alfabetojn.';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Vi elektis %d elŝutontajn bildojn, sed la sistemo limiĝas je %d. Vi povas modifi tiun aron aŭ la %d lastajn bildojn ne elŝutos.';
+$lang['real number of archives can differ'] = 'eblas, ke la reala nombro da arĥivoj malsamas';
+$lang['Download permissions'] = 'Elŝutaj permesiloj';
+$lang['Download set deleted'] = 'Elŝutaro forigita';
+$lang['Downloads'] = 'Elŝutoj';
+$lang['Edit the set'] = 'Modifi tiun aron';
+$lang['Estimated number of archives'] = 'Taksita nombro da arĥivoj';
+$lang['Estimated size'] = 'Taksita grandeco';
+$lang['Force the usage of PclZip instead of ZipArchive as ZIP library'] = 'Trudi la uzon de PclZip anstataŭ de ZipArchive kiel ZIP-biblioteko';
+$lang['Generate ZIP'] = 'Generi ZIP';
+$lang['It saves space on the server but doesn\'t allow to restart failed downloads.'] = 'Ŝparas spacon ĉe la servilo sed ne permesas la rekomenciĝon de fuŝitaj elŝutoj.';
+$lang['Maximum number of photos per download set'] = 'Maksimuma nombro da fotoj en ĉiu elŝutaro';
+$lang['Maximum photo size'] = 'Maksimuma bildograndeco';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Maksimuma grandeco de ĉiu arĥivo (en Mbajt)';
+$lang['No result'] = 'Neniu rezulto';
+$lang['Number of archives'] = 'Nombro da arĥivoj';
+$lang['Number of images'] = 'Nombro da fotoj';
+$lang['Only check if you are experiencing corrupted archives with ZipArchive.'] = 'Nur marku, se estas arĥivaj difektadoj uzante ZipArchive.';
+$lang['Only check if your host complains about high PHP usage.'] = 'Nur marku, se via gastigo plendas pri tro intensa uzo de PHP.';
+$lang['hours'] = 'horoj';
+$lang['Remove all finished downloads'] = 'Forigi ĉiujn finitajn elŝutojn';
+$lang['Remove from download set'] = 'Forigi el la elŝutaro';
+$lang['Return to download page'] = 'Reiri al elŝutopaĝo';
+$lang['Set type'] = 'Tipo de aro';
+$lang['Sorry, there is nothing to download. Some files may have been excluded because of <i title="Authorized types are : %s">filetype restrictions</i>.'] = 'Bedaŭron, nenio elŝuteblas. Eblas, ke kelkaj dosieroj eksigis pro <i title="Permesitaj tipoj estas: %s">limigoj pri la dosierotipo</i>.';
+$lang['Total size'] = 'Ĉioma grandeco';
+$lang['Unknown'] = 'Nekonata';
+$lang['User collection'] = 'Kolekto de uzanto';
+$lang['User groups'] = 'Grupoj da uzantoj';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Atentu: Nur registritaj uzantoj rajtas uzi la staplan elŝutilon';
+$lang['What can be downloaded?'] = 'Kion oni povas elŝuti?';
+$lang['Whole gallery'] = 'Tuta galerio';
+$lang['You can not edit this set'] = 'Vi ne povas modifi tiun aron';
+$lang['done'] = 'farita';
+$lang['download'] = 'elŝuto';
+$lang['%d MB'] = '%d Mbajt';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Atentu:</b>ĉiuj dosieroj estos forigitaj post %d horoj';
+$lang['Archive #%d (already downloaded)'] = 'Arĥivo #%d (jam elŝutita)';
+$lang['Archive #%d (pending)'] = 'Arĥivo #%d (pritraktata)';
+$lang['Archive #%d (ready)'] = 'Arĥivo #%d (preta)';
+$lang['Archive comment'] = 'Arĥiva komento';
+$lang['Archive prefix'] = 'Arĥiva prefikso';
+$lang['Archives'] = 'Arĥivoj';
+$lang['Batch Downloader'] = 'Stapla elŝutilo';
+$lang['Cancel this download'] = 'Nuligi tiun elŝuton';
+$lang['Cancel this set'] = 'Nuligi tiun aron';
+$lang['Confirm the download of %d pictures?'] = 'Ĉu konfirmi la elŝuton de %d bildoj?';
+$lang['Delete downloads after'] = 'Forigi la arĥivojn poste';
+$lang['Delete previous archive when starting to download another one'] = 'Forigi la antaŭan arĥivon, kiam komenciĝas la elŝuto de nova arĥivo';
+$lang['Delete this set'] = 'Forigi tiun aron';
+$lang['Don\'t download archives through PHP'] = 'Ne elŝuti arĥivojn per PHP';
+$lang['Download all pictures of this selection'] = 'Elŝuti ĉiujn bildojn en tiu selekto';
+$lang['Download history'] = 'Historio de la elŝutoj';
+$lang['Download info'] = 'Informoj pri la elŝuto';
+$lang['Download links'] = 'Elŝutaj ligiloj';
+?>
