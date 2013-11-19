8b1f90ad2d74b442fb437ddc8552c7b6744a5911
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-11-19 06:17:14 +0000

    [batch_downloader] Add vi_VN, thanks to : dummy
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@25572 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/vi_VN/plugin.lang.php
@@ -0,0 +1,85 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['hours'] = 'giờ';
+$lang['real number of archives can differ'] = 'số lượng lưu trữ thực có thể thay đổi';
+$lang['Remove all finished downloads'] = 'Xóa tất cả các tải xuống đã hoàn tất';
+$lang['Remove from download set'] = 'Xóa khỏi bộ tải xuống';
+$lang['Return to download page'] = 'Quay lại trang tải xuống';
+$lang['Set type'] = 'Thiết lập loại';
+$lang['Sorry, there is nothing to download. Some files may have been excluded because of <i title="Authorized types are : %s">filetype restrictions</i>.'] = 'Rất tiếc không có gì để tải xuống. Một số file đã bị loại bỏ vì  <i title="Các loại được phép là : %s">filetype restrictions</i>.';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Bắt đầu tải xuống lưu trữ #%d sẽ ghi đè lên lưu trữ #%d, hãy chắc là bạn muốn tải xuống. Tiếp tục ?';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Lưu trữ đang được tải xuống, nếu tải xuống không được tự động bắt đầu vui lòng <a href="%s">nhấp chuột vào đây</a>';
+$lang['Total size'] = 'Kích thước tổng cộng';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'Không tìm thấy phần mở rộng PHP ZipArchive, Trình tải xuống hàng loạt sẽ dùng PclZip nhưng sẽ bị giảm hiệu năng.';
+$lang['Unknown'] = 'Không biết';
+$lang['User collection'] = 'Bộ sưu tập người dùng';
+$lang['User groups'] = 'Nhóm người dùng';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Cảnh báo: Chỉ các người dùng đã đăng ký mới có thể dùng Trình tải xuống hàng loạt';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Cảnh báo: ZipArchive không chấp nhận các ký tự đặc biệt như ký tự có dấu, dấu nháy kép (") và các ký tự ngoài bảng latin.';
+$lang['What can be downloaded?'] = 'Những gì có thể tải xuống?';
+$lang['Whole gallery'] = 'Toàn bộ thư viện ảnh';
+$lang['You can not edit this set'] = 'Bạn không thể chỉnh sửa bộ này';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Bạn chọn tải xuống %d ảnh, nhưng hệ thống bị giới hạn ở mức %d. Bạn có thể chỉnh sửa bộ này, hoặc %d ảnh cuối sẽ không thể tải xuống.';
+$lang['done'] = 'hoàn tất';
+$lang['download'] = 'tải xuống';
+$lang['Download permissions'] = 'Quyền tải xuống';
+$lang['Download set deleted'] = 'Bộ tải xuống đã được xóa';
+$lang['Downloads'] = 'Tải xuống';
+$lang['Edit the set'] = 'Sửa bộ này';
+$lang['Estimated number of archives'] = 'Số lưu trữ ước tính';
+$lang['Estimated size'] = 'Kích thước ước tính';
+$lang['Force the usage of PclZip instead of ZipArchive as ZIP library'] = 'Buộc sử dụng PclZip thay cho ZipArchive làm thư viện ZIP';
+$lang['Generate ZIP'] = 'Xuất ra ZIP';
+$lang['It saves space on the server but doesn\'t allow to restart failed downloads.'] = 'Tiết kiệm không gian trên máy chủ nhưng không cho phép khởi động lại các tác vụ tải xuống bị hỏng.';
+$lang['Maximum number of photos per download set'] = 'Số ảnh tối đa cho mỗi bộ tải xuống';
+$lang['Maximum photo size'] = 'Kích thước ảnh tối đa';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Kích thước tối đa của mỗi lưu trữ (tính theo Megabytes)';
+$lang['No result'] = 'Không có kết quả';
+$lang['Number of archives'] = 'Số lưu trữ';
+$lang['Number of images'] = 'Số ảnh';
+$lang['Only check if you are experiencing corrupted archives with ZipArchive.'] = 'Chỉ đánh dấu nếu xảy ra hư hỏng với lưu trữ bằng ZipArchive.';
+$lang['Only check if your host complains about high PHP usage.'] = 'Chỉ đánh dấu nếu máy chủ cảnh báo về việc sử dụng PHP quá cao';
+$lang['Please wait, your download is being prepared. This page will automatically refresh when it is ready.'] = 'Vui lòng đợi trong khi tác vụ tải xuống được chuẩn bị. Trang này sẽ được tự động cập nhật khi sẵn sàng.';
+$lang['Preparation'] = 'Chuẩn bị';
+$lang['Random'] = 'Ngẫu nhiên';
+$lang['%d MB'] = '%d MB';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Cảnh báo:</b> tất cả file sẽ bị xóa trong vòng %d giờ';
+$lang['Archive #%d (already downloaded)'] = 'Lưu trữ #%d (đã tải xuống)';
+$lang['Archive #%d (pending)'] = 'Lưu trữ #%d (đang chờ)';
+$lang['Archive #%d (ready)'] = 'Lưu trữ #%d (sẵn sàng)';
+$lang['Archive comment'] = 'Bình luận của lưu trữ';
+$lang['Archive prefix'] = 'Tiền tố của lưu trữ ';
+$lang['Archives'] = 'Lưu trữ';
+$lang['Batch Downloader'] = 'Trình tải về hàng loạt';
+$lang['Cancel this download'] = 'Hủy tải xuống';
+$lang['Cancel this set'] = 'Hủy bộ này';
+$lang['Confirm the download of %d pictures?'] = 'Xác nhận tải xuống %d ảnh?';
+$lang['Delete downloads after'] = 'Xóa lưu trữ sau';
+$lang['Delete previous archive when starting to download another one'] = 'Xóa lưu trữ trước khi bắt đầu tải xuống tiếp theo';
+$lang['Delete this set'] = 'Xóa bộ này';
+$lang['Don\'t download archives through PHP'] = 'Không tải xuống lưu trữ qua PHP';
+$lang['Download all pictures of this selection'] = 'Tải xuống tất cả ảnh được chọn';
+$lang['Download history'] = 'Lịch sử tải xuống';
+$lang['Download info'] = 'Thông tin tải xuống';
+$lang['Download links'] = 'Liên kết tải xuống';
+?>
