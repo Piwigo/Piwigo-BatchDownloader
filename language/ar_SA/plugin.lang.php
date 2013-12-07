f048b531f8dd93511d0737e015581da907ff2905
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-07 19:55:13 +0000

    [batch_downloader] Update ar_SA, thanks to : aldambi
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@25825 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,43 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Download links'] = 'وصلات التحميل';
+$lang['Archives'] = 'الأرشيف';
+$lang['Download info'] = 'معلومات التحميل';
+$lang['Download history'] = 'تاريخ التحميل';
+$lang['Download all pictures of this selection'] = 'تحميل كل الصور في هذا الاختيار';
+$lang['Don\'t download archives through PHP'] = 'لا تحمل الارشيفات خلال بي اتش بي';
+$lang['Delete this set'] = 'حذف هذه المجموعة';
+$lang['Delete previous archive when starting to download another one'] = 'حذف الارشيفات السابقة عند بدء التحميل في المرة القادمة';
+$lang['Delete downloads after'] = 'حذف الارشيف بعد الانتهاء';
+$lang['Confirm the download of %d pictures?'] = 'تأكيد التحميل من  %d الصور ?';
+$lang['Cancel this set'] = 'إلغاء هذه المجموعة';
+$lang['Cancel this download'] = 'الغاء التنزيل';
+$lang['Batch Downloader'] = 'دفعة تنزيل';
+$lang['Archive prefix'] = 'بادئة أرشيف';
+$lang['Archive comment'] = 'ارشيف التعليق';
+$lang['Archive #%d (ready)'] = 'ارشيف #%d (جاهز)';
+$lang['Archive #%d (pending)'] = 'ارشيف #%d (ريثما)';
+$lang['%d MB'] = '%d MB';
+$lang['Archive #%d (already downloaded)'] = 'أرشيف  #%d (تحميلها بالفعل)';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>تحذير:</b> سيتم حذف جميع الملفات داخل %d ساعات ';
+?>
