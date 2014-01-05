81c31c1c30eeb8dcd64083968d5bac06aafaaac2
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-01-05 11:26:52 +0000

    [batch_downloader] Add sl_SI, thanks to : simon.krajnc
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@26464 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,40 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['%d MB'] = '%d MB';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Opozorilo:</b> vse datoteke bodo izbrisane v roku %d ur';
+$lang['Any size'] = 'Katerikoli velikost ';
+$lang['Archive #%d (already downloaded)'] = 'Archive #%d (already downloaded)';
+$lang['Archive #%d (pending)'] = 'Archive #%d (pending)';
+$lang['Archive #%d (ready)'] = 'Archive #%d (pripravljen)';
+$lang['Archives'] = 'Arhivi';
+$lang['Batch Downloader'] = 'Download skripta';
+$lang['Cancel this download'] = 'Prekini download';
+$lang['Cancel this set'] = 'Razveljavi ta sklop';
+$lang['Delete downloads after'] = 'Izbriši arhive po';
+$lang['Delete previous archive when starting to download another one'] = 'Izbriši prejšnji arhiv pred pričetkom downloada novega';
+$lang['Delete this set'] = 'Izbriši ta sklop';
+$lang['Don\'t download archives through PHP'] = 'Ne download archives through PHP';
+$lang['Download all pictures of this selection'] = 'Download vse označene slike';
+$lang['Download history'] = 'Download zgodovina';
+$lang['Download info'] = 'Download info';
+?>
