aa66e2efec5b13cc0a66d35c09fb9495a1f84ee6
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-03-05 18:09:35 +0000

    [batch_downloader] Update mn_MN, thanks to : Ulzii
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@27562 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/mn_MN/plugin.lang.php
@@ -0,0 +1,42 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Maximum photo size'] = 'Хамгийн том хэмжээ';
+$lang['One size'] = 'Тогтсон хэмжээтэй';
+$lang['User groups'] = 'Харэглэгчийн бүлэг';
+$lang['Download permissions'] = 'Татах эрх';
+$lang['What can be downloaded?'] = 'Татаж болох төрөл';
+$lang['Delete downloads after'] = 'Үүсгэсэн файлыг устгах хугацаа';
+$lang['Download history'] = 'Татцын бичлэг';
+$lang['Total size'] = 'Нийт хэмжээ';
+$lang['Number of images'] = 'Зургийн тоо';
+$lang['Cancel this download'] = 'Татацыг цуцлах';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Анхаар: Зөвхөн бүртгүүлсэн хэрэглэгчид Batch Downloader-г ашиглах боломжтой';
+$lang['Whole gallery'] = 'Цомогийг бүхэлд нь';
+$lang['hours'] = 'цаг';
+$lang['Generate ZIP'] = 'ZIP файл үүсгэх';
+$lang['Photo size'] = 'Зургын хэмжээ';
+$lang['Photo size choices'] = 'Хэмжээний сонголт';
+$lang['%d MB'] = '%d MB';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Сануулга:</b> Үүсгэсэн бүх файл %d цагын дараа устах болно.';
+$lang['Any size'] = 'Дурын хэмжээтэй';
+?>
