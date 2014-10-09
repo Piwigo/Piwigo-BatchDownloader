5a223b628d953c7afc310e9b61789cf5bbb24c74
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-10-09 19:41:48 +0000

    [batch_downloader] Update nb_NO, thanks to : paulen
    git-svn-id: http://piwigo.org/svn/extensions/BatchDownloader@29958 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/nb_NO/plugin.lang.php
@@ -0,0 +1,89 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['You can not edit this set'] = 'Du kan ikke endre dette settet';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'Du velger å laste ned%d bilder, men systemet er begrenset til%d. Du kan redigere settet, ellers vil ikke de siste%d bildene bli lastes ned.';
+$lang['done'] = 'ferdig';
+$lang['download'] = 'last ned';
+$lang['hours'] = 'timer';
+$lang['real number of archives can differ'] = 'reelle antall arkiver kan avvike';
+$lang['Photo size choices'] = 'Bilde størrelse valg';
+$lang['Please wait, your download is being prepared. This page will automatically refresh when it is ready.'] = 'Vennligst vent, din nedlasting forberedes. Denne siden vil automatisk oppdateres når den er klar.';
+$lang['Preparation'] = 'Forberedelse';
+$lang['Random'] = 'Tilfeldig';
+$lang['Remove all finished downloads'] = 'Fjern alle ferdige nedlastinger';
+$lang['Remove from download set'] = 'Fjern fra nedlasting settet';
+$lang['Return to download page'] = 'Gå tilbake til nedlastig siden';
+$lang['Set type'] = 'Sett type';
+$lang['Sorry, there is nothing to download. Some files may have been excluded because of <i title="Authorized types are : %s">filetype restrictions</i>.'] = 'Beklager, det er ingenting å laste ned. Noen filer kan ha blitt ekskludert på grunn av <i tittel = "de autoriserte typene er:%s">filtyper restriksjoner</i>.';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'Starter nedlasting av Arkiv#%d vil ødelegge Arkiv #%d, være sikker på at du er ferdig med nedlastingen. Fortsette?';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'Arkivet laster ned, hvis nedlastingen ikke starter automatisk kan du <a href="%s"> klikke her</a>';
+$lang['Total size'] = 'Total størrelse';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'Kan ikke finne ZipArchive PHP forlengelse, Batch Downloader vil bruke PclZip i stedet, men med dårligere ytelse.';
+$lang['Unknown'] = 'Ukjent';
+$lang['User collection'] = 'Bruker samling';
+$lang['User groups'] = 'Bruker gruppe';
+$lang['Warning: Only registered users can use Batch Downloader.'] = 'Advarsel: Bare registrerte brukere kan bruke Batch nedlaster.';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = 'Advarsel: ZipArchive godtar ikke spesialtegn som aksentuert, vinkel anførselstegn (») og ikke-latinske alfabeter.';
+$lang['What can be downloaded?'] = 'Hva kan lastes ned?';
+$lang['Whole gallery'] = 'Hele galleriet';
+$lang['Download links'] = 'Last ned lenker';
+$lang['Download permissions'] = 'Last ned tillatelser';
+$lang['Download set deleted'] = 'Last ned slettede sett';
+$lang['Downloads'] = 'Last ned';
+$lang['Edit the set'] = 'Redigere settet';
+$lang['Estimated number of archives'] = 'Beregnet antall arkiver';
+$lang['Estimated size'] = 'Beregn størrelse';
+$lang['Force the usage of PclZip instead of ZipArchive as ZIP library'] = 'Tving bruk av PclZip i stedet for ZipArkiv som ZIP bibliotek';
+$lang['Generate ZIP'] = 'Generer ZIP';
+$lang['It saves space on the server but doesn\'t allow to restart failed downloads.'] = 'Den sparer plass på serveren, men tillater ikke å starte en mislykkede nedlastinger.';
+$lang['Maximum number of photos per download set'] = 'Maksimalt antall bilder per nedlasting sett';
+$lang['Maximum photo size'] = 'Maksimal bilde størrelse';
+$lang['Maximum size of each archive (in Megabytes)'] = 'Maksimal størrelse på hvert arkiv (i megabyte)';
+$lang['No result'] = 'Ingen resultat';
+$lang['Number of archives'] = 'Antall arkiver';
+$lang['Number of images'] = 'Antall bilder';
+$lang['One size'] = 'En størrelse';
+$lang['Only check if you are experiencing corrupted archives with ZipArchive.'] = '
+Bare sjekk om du opplever ødelagte arkiver med ZipArchive.';
+$lang['Only check if your host complains about high PHP usage.'] = 'Sjekk bare hvis verten klager på høy PHP-bruk.';
+$lang['Photo size'] = 'Bilde størrelse';
+$lang['%d MB'] = '%d MB';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Advarsel:</b> alle filer vil bli slettet etter %d timer';
+$lang['Any size'] = 'Alle størrelser';
+$lang['Archive #%d (already downloaded)'] = 'Arkiv #%d (alerede nedlastet)';
+$lang['Archive #%d (pending)'] = 'Arkiv #%d (venter)';
+$lang['Archive #%d (ready)'] = 'Arkiv #%d (klar)';
+$lang['Archive comment'] = 'Arkiv kommentarer';
+$lang['Archive prefix'] = 'Arkiv prefiks';
+$lang['Archives'] = 'Arkiv';
+$lang['Batch Downloader'] = 'Batch Nedlaster';
+$lang['Cancel this download'] = 'Avbryt denne nedlastingen';
+$lang['Cancel this set'] = 'Avbryt dette settet';
+$lang['Confirm the download of %d pictures?'] = 'Bekreft nedlasting av%d bilder?';
+$lang['Delete downloads after'] = 'Slett arkiv etter';
+$lang['Delete previous archive when starting to download another one'] = 'Slett forige arkiv når en ny nedlasting starter';
+$lang['Delete this set'] = 'Slett dette settet';
+$lang['Don\'t download archives through PHP'] = 'Ikke last ned arkiver gjennom PHP';
+$lang['Download all pictures of this selection'] = 'Last ned alle bilder fra dette valget';
+$lang['Download history'] = 'Last ned historie';
+$lang['Download info'] = 'Last ned info';
