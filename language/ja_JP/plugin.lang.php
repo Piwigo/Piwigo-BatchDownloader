6fc9e986c786e270a7b91d3a6a3b00f0fc295750
Piwigo-TranslationTeam <translate-github@piwigo.org>
2019-07-17 06:34:00 +0200

    Add ja_JP, thanks to Trumpet

---
+++ language/ja_JP/plugin.lang.php
@@ -0,0 +1,74 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Cancel this set'] = 'この集合をキャンセルする';
+$lang['Delete this set'] = 'この集合を削除する';
+$lang['Download set deleted'] = '削除した集合をダウンロードする';
+$lang['Edit the set'] = '集合を編集する';
+$lang['Maximum number of photos per download set'] = 'ダウンロード集合の最大写真枚数';
+$lang['Remove from download set'] = 'ダウンロード集合から削除する';
+$lang['You can not edit this set'] = 'あなたはこの集合を編集できません';
+$lang['You choose to download %d pictures, but the system is limited to %d. You can edit the set, or the last %d pictures will not be downloaded.'] = 'あなたは %d 枚の写真をダウンロードするために選択しました、しかしシステムは %d 枚に制限しています。集合を編集しなければ、最後の %d 枚はダウンロードできないでしょう。';
+$lang['Whole gallery'] = '全てのギャラリー';
+$lang['done'] = '完了';
+$lang['download'] = 'ダウンロード';
+$lang['hours'] = '時間';
+$lang['real number of archives can differ'] = 'アーカイブの本当の数は異なることがあります';
+$lang['Photo size'] = '写真サイズ';
+$lang['Photo size choices'] = '選択した写真のサイズ';
+$lang['Please wait, your download is being prepared. This page will automatically refresh when it is ready.'] = 'ダウンロードは準備中なのでお待ち下さい。このページは準備されたら自動的にリフレッシュされるでしょう。';
+$lang['Preparation'] = '準備';
+$lang['Random'] = 'ランダム';
+$lang['Remove all finished downloads'] = '終了したダウンロード全てを削除する';
+$lang['Return to download page'] = 'ダウンロードページに戻る';
+$lang['Set type'] = 'タイプを設定する';
+$lang['Sorry, there is nothing to download. Some files may have been excluded because of <i title="Authorized types are : %s">filetype restrictions</i>.'] = '申し訳ありませんが、ダウンロードするものがありません。幾つかのファイルは、 <i title="許可されたタイプ : %s">ファイルタイプの制約</i>により除外されています。';
+$lang['Starting download Archive #%d will destroy Archive #%d, be sure you finish the download. Continue ?'] = 'アーカイブ #%d のダウンロードを開始すると、アーカイブ #%d を破損するでしょう。ダウンロードの完了を確認して下さい。続けますか？';
+$lang['The archive is downloading, if the download doesn\'t start automatically please <a href="%s">click here</a>'] = 'このアーカイブはダウンロード中です。もしダウンロードが自動的に開始しなければ、<a href="%s">ここをクリック</a>して下さい。';
+$lang['Total size'] = '全体サイズ';
+$lang['Unable to find ZipArchive PHP extension, Batch Downloader will use PclZip instead, but with degraded performance.'] = 'ZipArchive PHP拡張が見つかりません。バッチダウンローダーは代わりにPclZipを使いますが、性能が低下します。';
+$lang['Unknown'] = '未知';
+$lang['User collection'] = 'ユーザコレクション';
+$lang['User groups'] = 'ユーザグループ';
+$lang['Warning: Only registered users can use Batch Downloader.'] = '警告：登録されたユーザだけがバッチダウンローダーを使えます。';
+$lang['Warning: ZipArchive doesn\'t accept special characters like accentuated ones, angle quotes (») and non-latin alphabets.'] = '警告：ZipArchiveは、発音記号や＜＞記号、非ラテン文字などの特殊な文字は受け付けません。';
+$lang['What can be downloaded?'] = 'ダウンロードできるものは？';
+$lang['Download info'] = '情報をダウンロードする';
+$lang['Download links'] = 'リンクをダウンロードする';
+$lang['Download permissions'] = '権限をダウンロードする';
+$lang['Downloads'] = 'ダウンロードする';
+$lang['Estimated number of archives'] = 'アーカイブの推定数';
+$lang['Estimated size'] = '推定サイズ';
+$lang['Force the usage of PclZip instead of ZipArchive as ZIP library'] = 'ZIPライブラリとしてZipArchiveではなくPclZipを使うよう強いる';
+$lang['Generate ZIP'] = 'ZIPを生成する';
+$lang['It saves space on the server but doesn\'t allow to restart failed downloads.'] = 'サーバーの領域を節約するが、失敗したダウンロードの再開ができなくなる。';
+$lang['Maximum photo size'] = '写真の最大サイズ';
+$lang['Maximum size of each archive (in Megabytes)'] = '各アーカイブの最大サイズ（メガバイト）';
+$lang['No result'] = '結果がない';
+$lang['Number of archives'] = 'アーカイブの数';
+$lang['Number of images'] = '写真の枚数';
+$lang['One size'] = '1つのサイズ';
+$lang['Only check if you are experiencing corrupted archives with ZipArchive.'] = 'ZipArchiveで破損したアーカイブを経験したかどうかだけを検査する';
+$lang['Only check if your host complains about high PHP usage.'] = 'ホストが高いPHP使用に不平を言うかどうかだけを検査する';
+$lang['%d MB'] = '%d MB';
+$lang['%s plugin detected, albums will be downloadable according to permissions.'] = '%s プラグインが検出されました。アルバムは、権限に応じてダウンロード可能になるでしょう。';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>警告：</b> 全てのファイルは %d 時間以内に消去されるでしょう';
+$lang['Any size'] = 'サイズを問わず';
+$lang['Archive #%d (already downloaded)'] = 'アーカイブ #%d (既にダウンロード済）';
+$lang['Archive #%d (pending)'] = 'アーカイブ #%d （保留）';
+$lang['Archive #%d (ready)'] = 'アーカイブ #%d （準備）';
+$lang['Archive comment'] = 'アーカイブコメント';
+$lang['Archive prefix'] = 'アーカイブのプレフィクス';
+$lang['Archives'] = 'アーカイブ';
+$lang['Batch Downloader'] = 'バッチダウンローダ';
+$lang['Cancel this download'] = 'このダウンロードをキャンセルする';
+$lang['Confirm the download of %d pictures?'] = '%d 枚の写真をダウンロードしますか？';
+$lang['Delete downloads after'] = '後でアーカイブを削除する';
+$lang['Delete previous archive when starting to download another one'] = '他のダウンロードを開始したら以前のアーカイブを削除する';
+$lang['Don\'t download archives through PHP'] = 'PHPを通してアーカイブをダウンロードしない';
+$lang['Download all pictures of this selection'] = '選択した全ての写真をダウンロードする';
+$lang['Download history'] = '履歴をダウンロードする';
