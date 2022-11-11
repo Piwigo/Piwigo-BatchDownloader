8dae7d66b3ca7aeca95c355c8090fe9d932086bb
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-11-11 10:13:15 +0100

    Update es_ES, thanks to juanklopez

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -87,3 +87,4 @@ $lang['One size'] = 'Un formato';
 $lang['Photo size'] = 'Formato de la foto';
 $lang['Photo size choices'] = 'Eligir el formato de la foto';
 $lang['%s plugin detected, albums will be downloadable according to permissions.'] = '%s plugin detectado, álbumes serán descargable según los permisos.';
+$lang['%s lines printed, %s in total.'] = '%s líneas impresas, %s en total.';
