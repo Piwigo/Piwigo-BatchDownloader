7f20c5ad23b55d30031dd1487117dc9880b41da5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-03-13 06:53:54 +0100

    Update fr_CA, thanks to bgoutier

--- language/fr_CA/plugin.lang.php
+++ language/fr_CA/plugin.lang.php
@@ -7,3 +7,15 @@
 // +-----------------------------------------------------------------------+
 $lang['Maximum size of each archive (in Megabytes)'] = 'Taille maximale de chaque archive (en Mégaoctets)';
 $lang['%s lines printed, %s in total.'] = '%s lignes imprimées, %s au total.';
+$lang['Your download request has been sent'] = 'Votre demande de téléchargement a été envoyée. Un administrateur va traiter votre demande. Vous recevrez prochainement un courriel afin de télécharger les photos.';
+$lang['This is used in the email sent when a users download request is accepted.'] = 'Ceci est utilisé dans le courriel envoyé lorsqu\'une demande de téléchargement d\'un utilisateur est acceptée.';
+$lang['This isn\'t a valid email, please try again'] = 'Ce courriel n\'est pas valide, veuillez réessayer';
+$lang['This isn\'t the correct format for an email'] = 'Ce n\'est pas le bon format pour courriel';
+$lang['Please fill out your Organisation'] = 'Veuillez spécifier votre nom de famille';
+$lang['Please fill out your email'] = 'Merci de spécifier votre courriel';
+$lang['Please fill out your profession'] = 'Merci de spécifier votre numéro de téléphone';
+$lang['Please fill out your telephone number'] = 'SVP spécifier votre numéro de téléphone';
+$lang['Please provide an email address'] = 'Veuillez fournie une adresse courriel';
+$lang['Email'] = 'Adresse courriel';
+$lang['Please fill out your First name'] = 'Veuillez spécifier votre prénom';
+$lang['Please fill out your Last name'] = 'Veuillez spécifier votre nom de famille';
