b7d62c72eafad7ce9af58e76ddf841007d9af186
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-11-23 06:30:37 +0100

    Update fi_FI, thanks to maakuth

---
+++ language/fi_FI/plugin.lang.php
@@ -0,0 +1,50 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Force the usage of PclZip instead of ZipArchive as ZIP library'] = 'Pakota PclZip-kirjasto ZipArchiven asemesta';
+$lang['Here are the details of the request:'] = 'Tässä ovat pyynnön yksityiskohdat:';
+$lang['Generate ZIP'] = 'Luo ZIP';
+$lang['History'] = 'Historia';
+$lang['Last name'] = 'Sukunimi';
+$lang['For more details or information, please %scontact the administrator%s.'] = 'Lisätietoja saat ottamalla yhteyttä %s ylläpitäjiin %s.';
+$lang['Estimated size'] = 'Arvioitu koko';
+$lang['First name'] = 'Etunimi';
+$lang['Download info'] = 'Lataustiedot';
+$lang['Download links'] = 'Latauslinkit';
+$lang['Download permissions'] = 'Latausoikeudet';
+$lang['Download request'] = 'Latauspyyntö';
+$lang['Download requests'] = 'Latauspyynnöt';
+$lang['Download set deleted'] = 'Lataussetti poistettu';
+$lang['Downloads'] = 'Lataukset';
+$lang['Edit the set'] = 'Muokkaa settiä';
+$lang['Email'] = 'Sähköposti';
+$lang['Estimated number of archives'] = 'Arvioitu arkistojen määrä';
+$lang['Confirm the download of %d pictures?'] = 'Vahvista %d kuvan lataus?';
+$lang['Delete downloads after'] = 'Poista arkistot tämän ajan kuluttua';
+$lang['Delete previous archive when starting to download another one'] = 'Poista edellinen arkisto kun seuraavan lataus alkaa';
+$lang['Delete this set'] = 'Poista tämä setti';
+$lang['Don\'t download archives through PHP'] = 'Älä poista arkistoja PHP:n kautta';
+$lang['Download all pictures of this selection'] = 'Poista kaikki valinnan kuvat';
+$lang['Download history'] = 'Lataushistoria';
+$lang['Batch Downloader'] = 'Sarjalatain';
+$lang['Batch downloader, new download request '] = 'Sarjalatain, uusi latauspyyntö';
+$lang['Batch downloader, your request has been processed'] = 'Sarjalatain, pyyntösi on käsitelty';
+$lang['Cancel this download'] = 'Peru tämä lataus';
+$lang['Cancel this set'] = 'Peru tämä setti';
+$lang['As a reminder, you agree to accept the general conditions of use and to respect the rights relating to intellectual property.'] = 'Muistutuksena, hyväksyt yleiset käyttöehdot ja kunnioitat tekijänoikeussääntöjä.';
+$lang['Accepted by'] = 'Hyväksynyt';
+$lang['Any size'] = 'Mikä tahansa koko';
+$lang['Archive #%d (already downloaded)'] = 'Arkisto #%d (jo ladattu)';
+$lang['Archive #%d (pending)'] = 'Arkisto #%d (odottaa)';
+$lang['Archive #%d (ready)'] = 'Arkisto #%d (valmis)';
+$lang['Archive comment'] = 'Arkistoi kommentti';
+$lang['Archive prefix'] = 'Arkistoi etuliite';
+$lang['Archives'] = 'Arkistot';
+$lang['<b>Warning:</b> all files will be deleted within %d hours'] = '<b>Varoitus:</b> kaikki tiedostoto poistetaan %d tunnin kuluessa';
+$lang['%s lines printed, %s in total.'] = '%s riviä tulostettu, %s riviä yhteensä';
+$lang['%d MB'] = '%d MT';
+$lang['%s plugin detected, albums will be downloadable according to permissions.'] = 'Liitännäinen %s todettu, albumeita voi ladata käyttöoikeuksien mukaisesti.';
